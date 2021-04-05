668d09b9ad63009d48c81d5436d89aa50a30921c
palakpant <39594597+palakpant@users.noreply.github.com>
2021-04-05 07:33:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource2);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "♕ Player 1 Wins!";
 }
- if(randomNumber2 > randomNumber1) {
+else if(randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = " Player 2 Wins! ♔";
 }
-if(randomNumber2===randomNumber1)
+else
 {
   document.querySelector("h1").innerHTML = "It's a TIE! ";
 }
